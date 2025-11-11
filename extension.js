28f18bca9b73daf056ce4f0be2176ba791c65aaf
n0tjohnny <jmarcoscassol@gmail.com>
2025-11-11 19:00:57 -0300

    update

--- extension.js
+++ extension.js
@@ -1,5 +1,23 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+   * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   * GNU General Public License for more details.
+   *
+   * You should have received a copy of the GNU General Public License
+   * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   *
+   * SPDX-License-Identifier: GPL-2.0-or-later
+   */
 import {Extension} from "resource:///org/gnome/shell/extensions/extension.js";
 import Meta from "gi://Meta";
+import Shell from "gi://Shell";
 import St from "gi://St";
 
 export default class TrimmerExtension extends Extension {
@@ -61,10 +79,10 @@ export default class TrimmerExtension extends Extension {
   }
 
   enable() {
+    // Use default settings without custom ID
     this._settings = this.getSettings();
     this._clipboard = St.Clipboard.get_default();
     
-    // Updated for GNOME 49 compatibility
     const display = global.display;
     this._selection = display.get_selection();
     
